1faf18bae252a401975b8b662d481bd3101cf017
Iyyaz Baber <iyyaz.baber@cobrasphere.com>
2021-03-23 00:15:03 +0000

    Removing MySQL DB
    On branch remove-db
    Changes to be committed:
    	modified:   .env.example
    	modified:   Dockerfile
    	modified:   docker-compose.yml
    	modified:   index.php

--- index.php
+++ index.php
@@ -4,14 +4,14 @@ try {
     echo 'Current PHP version: ' . phpversion();
     echo '<br />';
 
-    $host = 'db';
-    $dbname = 'database';
-    $user = 'user';
-    $pass = 'pass';
-    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
-    $conn = new PDO($dsn, $user, $pass);
+    // $host = 'db';
+    // $dbname = 'database';
+    // $user = 'user';
+    // $pass = 'pass';
+    // $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
+    // $conn = new PDO($dsn, $user, $pass);
 
-    echo 'Database connected successfully';
+    // echo 'Database connected successfully';
     echo '<br />';
 } catch (\Throwable $t) {
     echo 'Error: ' . $t->getMessage();
